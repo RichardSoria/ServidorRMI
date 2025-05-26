cdefd84ecfdcbed0322735d11b69f4f8000cd4c8
APP DISTRIBUIDAS <richard.soria@epn.edu.ec>
2025-05-26 16:50:56 -0500

    Initial commit

---
+++ src/rmi/servidor/test/Test.java
@@ -0,0 +1,18 @@
+package Test;
+
+import rmi.servidor.clase.Servidor;
+import rmi.servidor.clase.ServidorImpl;
+
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        LocateRegistry.createRegistry(1099);
+        ServidorImpl servidor = new ServidorImpl();
+        String rmiObjectName = "rmi://localhost/Datos";
+        Naming.rebind(rmiObjectName, servidor);
+        System.out.println("Servidor RMI iniciado y esperando peticiones...");
+    }
+}
